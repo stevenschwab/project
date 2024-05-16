f40e4bd7dcc4323d0762503ee72e047a92a43d85
Steve Schwab <schwabs@umich.edu>
2024-05-16 16:42:14 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+server.get('/hello', (req, res) => {
+    res.json('hello, there')
+})
+
+const PORT = process.env.PORT || 9000
+
+server.listen(PORT, () => {
+    console.log(`listening on PORT ${PORT}`)
+})
